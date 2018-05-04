2e0b33db247acb086bbec400976f69ccfbfbe435
Cliffton Fernandes <cliffton.fernandes.rulz@gmail.com>
2018-05-03 20:52:23 -0400

    - exchange enum added.

--- code/src/exchange.h
+++ code/src/exchange.h
@@ -5,4 +5,18 @@
 #ifndef CODE_EXCHANGE_H
 #define CODE_EXCHANGE_H
 
+namespace amqp {
+    class exchange {
+
+        enum type {
+            DIRECT,
+            FAN_OUT,
+            TOPIC
+        };
+
+    private:
+        type type_;
+    };
+}
+
 #endif //CODE_EXCHANGE_H
