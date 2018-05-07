afc6663f72d55c7b8f675fe5597d30e7bef97179
manpkaur <mk3646@g.rit.edu>
2018-05-06 22:41:12 -0400

    finished commenting

--- code/src/broker.cpp
+++ code/src/broker.cpp
@@ -1,40 +1,55 @@
 #include "broker.h"
 
 using amqp::broker;
-bool broker::is_running = true;
 
+/*
+ * Construct broker with input exchange type
+ * @param: exchange type
+ */
 broker::broker(exchange::type etype) : exchange_{etype} {
     std::cout << "Broker created!" << std::endl;
 }
 
-void broker::run() {
 
-    std::cout << "Running " << std::endl;
-    while (broker::is_running) {
-        message msg = message("data", "x.x");
-        exchange_.process_message(msg);
-    }
-
-}
-
+/*
+ * Remove the binding corresponding to input binding key
+ * @param: input binding key
+ */
 void amqp::broker::endBinding(string i_bindingKey) {
     exchange_.endBinding(i_bindingKey);
 }
 
+/*
+ * end simulation method will be called once main thread has exited.
+ * It will remove all the binding and queues.
+ */
 void amqp::broker::endSimulation() {
     exchange_.endSession();
 
 }
-
+/*
+* publish the input message based on the message type and exchange type
+*
+* @param msg: input Message
+*
+*/
 void broker::publish(message msg) {
     exchange_.process_message(msg);
 }
 
+/*
+* Register all possible binding entered by user
+*/
 
 void broker::register_binding(const string &binding_key) {
     exchange_.bind(binding_key);
 }
 
+/*
+* Register input client
+* @param client: input client
+* @param binding_key: input binding key
+*/
 std::shared_ptr<amqp_queue> broker::register_client(client &c, const string &binding_key) {
     return exchange_.bind_client(c, binding_key);
 }
--- code/src/broker.h
+++ code/src/broker.h
@@ -30,26 +30,53 @@ namespace amqp {
      *
      */
     class broker {
+
     public:
+        /*
+         * Construct broker with input exchange type
+         * @param: exchange type
+         */
         broker(exchange::type etype);
+
+        /*
+        * Register all possible binding entered by user
+        */
 
         void register_binding(const string &binding_key);
 
+        /*
+        * Register input client
+        * @param client: input client
+        * @param binding_key: input binding key
+        */
+
         std::shared_ptr<amqp_queue> register_client(client &c, const string &binding_key);
 
+        /*
+         * publish the input message based on the message type and exchange type
+         *
+         * @param msg: input Message
+         *
+         */
 
         void publish(message msg);
 
-        void run();
 
-        static bool is_running;
-
+        /*
+         * end simulation method will be called once main thread has exited.
+         * It will remove all the binding and queues.
+         */
         void endSimulation();
 
+        /*
+         * Remove the binding corresponding to input binding key
+         * @param: input binding key
+         */
         void endBinding(string i_bindingKey);
 
     private:
 
+        //Exchange associated with the broker.
         exchange exchange_;
 
 
